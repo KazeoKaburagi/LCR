17158f6acb676826ce65c371e799010979fb5ddf
KazeoKaburagi <stvg1696@gmail.com>
2018-04-26 15:55:59 +0700

    navbar

---
+++ index.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en" id="home">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link href="asset/lib/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="asset/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>My Profile?</title>
+  </head>
+  <body>
+<!--slider-->
+    <section class="slider">
+        <div class="box-slider">
+               
+            <?php include('inc/header.php'); ?>
+            
+            <div class="line"></div>
+            
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Wrapper for slides -->
+                <div class="carousel-inner">
+                    <div class="item active">
+                        <img src="image/bg-cover.png" alt="slider">
+                        <div class="caption">
+                            <div class="title">WELCOME TO PT. PUSPETINDO</div>
+                            <div class="subtitle">We’re the name you can trust<br>
+                                when you expect above the average quality and<br>
+                                large scale manufacturing capability
+                            </div>
+                            <div class="tombol">
+                                <img src="asset/images/button-left.png" class="left" alt="left">
+                                <img src="asset/images/button-right.png" class="right" alt="right">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="item">
+                        <img src="image/bg-cover.png" alt="slider">
+                        <div class="caption">
+                            <div class="title">WELCOME TO PT. PUSPETINDO</div>
+                            <div class="subtitle">We’re the name you can trust<br>
+                                when you expect above the average quality and<br>
+                                large scale manufacturing capability
+                            </div>
+                            <div class="tombol">
+                                <img src="asset/images/button-left.png" class="left" alt="left">
+                                <img src="asset/images/button-right.png" class="right" alt="right">
+                            </div>
+                        </div>
+                    </div>
+                </div>   
+            </div>
+        </div>
+</section>
+
+<!--akhir slider-->
+   
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="asset/js/jquery-3.3.1.min.js"></script>
+    <script src="asset/js/script.js"></script>
+  </body> 
+</html>
