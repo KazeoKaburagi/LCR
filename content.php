3890a3f35940b1540d23562e83f16e94b5b28211
kikiinc75 <wahyuiqbal91@gmail.com>
2018-04-26 15:51:43 +0700

    content & category

---
+++ content.php
@@ -0,0 +1,88 @@
+        <section class="ask">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8">
+                        <div class="description">Our experienced sales team is ready for you, please let us know about your request. Click on the enquire button and one of our staff will respond to you right away.</div>
+                    </div>
+                    <div class="col-md-4 text-right">
+                        <button class="btn btn-default">ENQUIRE NOW</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <div class="title">
+                            WE BUILD OUR<br>REPUTATIONS
+                        </div>
+                        <div class="subtitle">
+                            AS NATION LEADING PRECIPITATED CALCIUM CARBONATE FACTORY
+                        </div>
+                        <div class="description">
+                            SINCE 1979
+                        </div>
+                        <img src="image/imgcontent.jpg" alt="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 border">
+                        <div class="left-content">
+                            AS A LEADING PRECIPITATED CALCIUM CARBONATE FACTORY / PRODUCER, PT. LIGHT CALSINDO RAYA (LCR) CONTINUES TO EDUCATE AND PENETRATE THE MARKET WITH KNOWLEDGE OF THE IMPORTANCE TO ONLY USE THE HIGH QUALITY GRADE WHEN IT COMES TO PRECIPITATED CALCIUM CARBONATE.
+                            <br><br>
+                            PT. Light Calsindo Raya offers its customers a wide range of product customisation from basic Precipitated Calcium Carbonate to specifically adjusted ingredients calcium carbonate. With our factory at Sidoarjo, East Java and network distribution that spreads all over the major cities of Indonesia, PT. Light Calsindo Raya is one of the leaders of Precipitated Calcium Carbonate producer in Indonesia.
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="right-content">
+                            It is our culture to only bring our customers the best quality products. Our company promise to bring meticulously tested quality together with guaranteed volume availability of calcium products. We put competence and dedication in the areas of research and development to bring attractive and competitive product price with long-term stability. 
+                            <br><br>
+                            PT. Light Calsindo Raya secure its' customers through a clear business strategy and decisive, future-oriented investments in technology. We will do everything in our power for our employees and our customers.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="category">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="image/content1.jpg" alt="">
+                        <div class="title-category">
+                            PRODUCTS & QUALITY
+                        </div>
+                        <div class="subtitle-category">
+                            High quality products for a demanding market
+                        </div>
+                        <div class="link">
+                            <i class="glyphicon glyphicon-chevron-right"> </i>&nbsp;<a href="">LEARN MORE</a>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="image/content2.jpg" alt="">
+                        <div class="title-category">
+                            PRODUCTION FACILITY
+                        </div>
+                        <div class="subtitle-category">
+                            Our modern facilities and our human resources
+                        </div>
+                        <div class="link">
+                            <i class="glyphicon glyphicon-chevron-right"> </i>&nbsp;<a href="">LEARN MORE</a>                            
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="image/content3.jpg" alt="">
+                        <div class="title-category">
+                            CAREER
+                        </div>
+                        <div class="subtitle-category">
+                            Working together, developing together
+                        </div>
+                        <div class="link">
+                            <i class="glyphicon glyphicon-chevron-right"> </i>&nbsp;<a href="">LEARN MORE</a>                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
