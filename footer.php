af96b2174b1d10bf5f3f5d44f5b825208f790590
unknown <dedsec11>
2018-04-30 10:48:39 +0700

    footer

---
+++ footer.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en" id="home">
+ <head>
+   <!-- Required meta tags -->
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+   <!-- Bootstrap CSS -->
+   <link href="asset/lib/bootstrap.min.css" rel="stylesheet">
+   <link rel="stylesheet" href="asset/lib/style.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+   <title>LCR</title>
+ </head>
+ <body>
+<!--client-->
+<section class="client">
+    <div class="container">
+         <p>OUR NOTABLE CLIENTS</p>
+          <div class="row text-center">
+                    <div class="col-sm-15">
+                        <img src="asset/pertamina2.jpg">
+                    </div>
+                    <div class="col-sm-15">
+                        <img src="asset/dongi.jpg">
+                    </div>
+                    <div class="col-sm-15">
+                        <img src="asset/puspri2.jpg">
+                    </div>    
+                    <div class="col-sm-15">
+                        <img src="asset/enersol2.jpg">
+                    </div>    
+                    <div class="col-sm-15">
+                        <img src="asset/semen.jpg">
+                    </div>
+            
+        </div>
+    </div>
+</section>
+<!---akhir client-->
+
+<!--footer-->
+<footer class="foot">
+    <div class="container">
+        <div class="row">
+           <div class="text-left">
+                <img src="asset/logo-bot.jpg">
+                <div class="subtitle">PT.Light Casindo Raya Indonesia is a Preciptated calcium Carbonate factory operating from Sidoarjo, East Java - Indonesia.<br>Our main competitive factor iles in the quality of our product. which produced by modern and state-of-the-art facilities.</div>
+           </div>
+        
+           <div class="text-right">
+                <p>CONTACT INFO</p>
+                 <div class="line"></div>
+                 <i class="fa fa-phone">&nbsp &nbsp +62 031-99103048</i>
+                 <div class="fa fa-envelope">&nbsp &nbsp info@calsindo.co.id</div>
+                 <div class="title">copyright<div class="fa fa-copyright"></div>2018 PT.Light Calsindo Raya, Indonesia. All Right Reserved<br>WEbsite by Mark Design</div>
+            </div>
+           </div>
+        </div>
+</footer>
+
+
+   <!-- Optional JavaScript -->
+   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+   <script src="asset/js/jquery-3.3.1.min.js"></script>
+   <script src="asset/js/script.js"></script>
+ </body> 
+</html>
